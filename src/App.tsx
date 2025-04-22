8ceb08e20115dc0db5a143da306954c704301ce9
DJ <gk22719@gmail.com>
2025-04-22 16:34:39 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <div className="bg-blue-100 p-4 text-white text-center font-bold text-4xl">Mood Journal</div>
+        </>
+    );
+}
+
+export default App;
